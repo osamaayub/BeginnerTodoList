0b63939238c48cd02025545c07d637aeb7c4b8a5
Osama ayub <ayubosama416@gmail.com>
2023-06-22 20:25:30 +0500

    todo-list project added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoWrapper from './components/TodoWrapper';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <TodoWrapper/>
+    </div>
+  )
+}
+
+export default App
